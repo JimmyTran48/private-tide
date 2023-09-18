d74e1d7b965d12c26010eef16782e8c9dfff45be
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-17 19:41:44 -0500

    Initial Boilerplate Commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, './client/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    hot: true,
+    historyApiFallback: true,
+    static: {
+      publicPath: '/dist',
+      directory: path.resolve(__dirname, '/dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
