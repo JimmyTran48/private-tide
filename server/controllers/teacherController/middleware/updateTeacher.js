0dc99a90f7282dd717d2afaba3d1e4f9e9467af0
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-19 15:36:51 -0500

    Boilerplate for updateTeacher middleware

---
+++ server/controllers/teacherController/middleware/updateTeacher.js
@@ -0,0 +1,22 @@
+/**
+ * **************************************************
+ *
+ * @module teacherController.updateTeacher
+ *
+ * @description
+ * Updates an entry in the teacher table.
+ *
+ * **************************************************
+ */
+
+const db = require('../../../database');
+
+/**
+ * ====================================
+ *        MIDDLEWARE FUNCTION
+ * ====================================
+ */
+
+const updateTeacher = async (req, res, next) => {};
+
+module.exports = updateTeacher;
