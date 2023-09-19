5d03e41785477b8c0bb77045f8f79e3dc9576e8f
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-18 22:25:41 -0500

    Initialized schoolController

---
+++ server/controllers/schoolController/index.js
@@ -0,0 +1,14 @@
+/**
+ * **************************************************
+ *
+ * @module schoolController
+ *
+ * @description
+ * schoolController is used to create and manage schools.
+ *
+ * **************************************************
+ */
+
+module.exports = {
+  
+}
