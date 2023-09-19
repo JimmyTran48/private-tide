7b6dba8cf1498406318f1d8b0afe8916a27a9021
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-18 21:30:49 -0500

    Initial server commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+//  routes
+const api = require('./routes/api');
+
+app.use('/api', api);
+
+app.get('/*', (_, res) => {
+  return res.sendFile(path.resolve(_ - path.dirname, '../dist/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.message);
+  res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
