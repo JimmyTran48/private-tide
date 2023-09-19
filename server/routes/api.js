a56aecc79be49de36e51c65a041744f47f888547
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-18 21:37:25 -0500

    Initialized API router

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+//  routes
+const teachers = require('./teachers');
+const schools = require('./schools');
+const students = require('./students');
+const lessons = require('./lessons');
+const sessions = require('./sessions');
+
+router.use('/teachers', teachers);
+router.use('/schools', schools);
+router.use('/students', students);
+router.use('/lessons', lessons);
+router.use('/sessions', sessions);
+
 module.exports = router;
