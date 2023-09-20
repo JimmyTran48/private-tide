b0c2b80a0fc554027d812fc287455670908671c4
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-19 23:06:35 -0500

    Updated teachers route

--- server/routes/teachers.js
+++ server/routes/teachers.js
@@ -2,4 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const authController = require('../controllers/authController');
+const teacherController = require('../controllers/teacherController');
+
+router.post(
+  '/',
+  authController.hashPassword,
+  teacherController.createTeacher,
+  (req, res) => {
+    return res.sendStatus(201);
+  }
+);
+
+router.patch('/', teacherController.updateTeacher, (req, res) => {
+  return res.sendStatus(204);
+});
+
 module.exports = router;
