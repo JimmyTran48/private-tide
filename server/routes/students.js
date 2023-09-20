62d310d0b5bfb1704b7e189fff2f663ad4ff7ca1
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-20 16:43:29 -0500

    Finished students router

--- server/routes/students.js
+++ server/routes/students.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const studentController = require('../controllers/studentController');
+
+router.post('/', studentController.createStudent, (req, res) => {
+  return res.sendStatus(201);
+});
+
+router.get('/:teacher_id', studentController.getStudents, (req, res) => {
+  return res.status(200).json(res.locals.students);
+});
+
+router.patch('/:id', studentController.updateStudent, (req, res) => {
+  return res.sendStatus(204);
+});
+
 module.exports = router;
