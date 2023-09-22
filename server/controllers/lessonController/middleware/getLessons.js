a4b1e3aaf77376dc56af835fce89a18bc629fc4c
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-21 20:12:44 -0500

    Completed lesson router

--- server/controllers/lessonController/middleware/getLessons.js
+++ server/controllers/lessonController/middleware/getLessons.js
@@ -21,15 +21,16 @@ const getlessons = async (req, res, next) => {
   const { teacher_id } = req.params;
 
   const query = `
-  SELECT student.name, lesson.lesson_date, lesson.price, lesson.payment_status
+  SELECT student.first_name, student.last_name, lesson.lesson_date, lesson.price, lesson.payment_status
   FROM lessons AS lesson
   JOIN students AS student ON lesson.student_id = student.id
-  WHERE teacher_id = $1
+  WHERE lesson.teacher_id = $1
   `;
   const params = [teacher_id];
 
   const lessons = await db.query(query, params);
 
+  console.log(lessons.rows);
   res.locals.lessons = lessons.rows;
 
   return next();
--- server/routes/lessons.js
+++ server/routes/lessons.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const lessonController = require('../controllers/lessonController');
+
+router.post('/', lessonController.createLesson, (req, res) => {
+  return res.sendStatus(201);
+});
+
+router.get('/:teacher_id', lessonController.getLessons, (req, res) => {
+  return res.status(200).json(res.locals.lessons);
+});
+
+router.patch('/:id', lessonController.updateLesson, (req, res) => {
+  return res.sendStatus(204);
+});
+
 module.exports = router;
