6e062aef27da52bf895429677c711f9da9b14187
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-22 21:26:44 -0500

    Initial setup of React Router

---
+++ client/App.js
@@ -0,0 +1,35 @@
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+import SchoolsPage from './pages/SchoolsPage';
+import StudentsPage from './pages/StudentsPage';
+import UserHomePage from './pages/UserHomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootPage />}>
+      {/* Public Routes */}
+      <Route exact path='/' element={HomePage} />
+
+      {/* Authenticated Routes */}
+      <Route path='/home' element={UserHomePage} />
+      <Route path='/students' element={StudentsPage} />
+      <Route path='/schools' element={SchoolsPage} />
+
+      {/* 404 Page */}
+      <Route component={NotFound} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
