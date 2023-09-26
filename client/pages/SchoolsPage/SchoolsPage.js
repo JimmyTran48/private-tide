9f679bac2d31143542b5707243c581467c5b5e40
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-09-26 10:20:10 -0500

    Updated SchoolsPage

--- client/pages/SchoolsPage/SchoolsPage.js
+++ client/pages/SchoolsPage/SchoolsPage.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SchoolsPage = () => { 
-  return <h2>Schools Page</h2>
+import StudentCount from '../../components/StudentCount';
+
+import schoolsData from '../../data/schools';
+
+const SchoolsPage = () => {
+  const [schools] = useState(schoolsData);
+  
+  return (
+    <div className='container mx-auto p-4'>
+      <h2 className='text-2xl font-bold mb-4'>Schools</h2>
+      <div className='grid grid-cols-2 gap-4'>
+        {schools.map((school, index) => (
+          <StudentCount key={index} schoolName={school.name} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SchoolsPage;
