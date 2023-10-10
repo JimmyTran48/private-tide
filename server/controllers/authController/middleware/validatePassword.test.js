5e48b6262ff9aef770fbb9b533c3e75e42ca56ab
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-10-09 23:07:10 -0500

    Test authController validatePassword middleware

---
+++ server/controllers/authController/middleware/validatePassword.test.js
@@ -0,0 +1,62 @@
+const validatePassword = require('./validatePassword');
+const bcrypt = require('bcrypt');
+const db = require('../../../database');
+
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(() => Promise.resolve()),
+}));
+
+jest.mock('../../../database', () => ({
+  query: jest.fn(() => {
+    return { rows: [{ password: 'hashed password' }] };
+  }),
+}));
+
+describe('Testing authController validatePassword middleware', () => {
+  it('should handle a correct password', async () => {
+    const req = {
+      body: {
+        password: 'testPassword',
+      },
+    };
+    const res = {
+      locals: {},
+    };
+    const next = jest.fn();
+
+    bcrypt.compare.mockReturnValueOnce(true);
+
+    await validatePassword(req, res, next);
+
+    expect(db.query).toHaveBeenCalled();
+    expect(bcrypt.compare).toHaveBeenCalledWith(
+      'testPassword',
+      'hashed password'
+    );
+    expect(res.locals.teacher).toEqual({ password: 'hashed password' });
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should handle an incorrect password', async () => {
+    const req = {
+      body: {
+        password: 'testPassword',
+      },
+    };
+    const res = {
+      locals: {},
+    };
+    const next = jest.fn();
+
+    bcrypt.compare.mockReturnValueOnce(false);
+
+    await validatePassword(req, res, next);
+
+    expect(res.locals.teacher).toBeUndefined();
+    expect(next).toHaveBeenCalledWith({
+      log: 'authController, validatePassword middleware',
+      status: 401,
+      message: 'Incorrect username or password',
+    });
+  });
+});
